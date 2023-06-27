308f2da47f1bd67f825a34bd1a4bfc3304bbe4c6
azilRababe <71320517+azilRababe@users.noreply.github.com>
2023-06-27 22:32:11 +0100

    Delete backend directory

--- backend/models/genre.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const genreSchema = mongoose.Schema({
-  genre: { type: String, required: true, trim: true },
-});
-
-const Genre = mongoose.model("Genre", genreSchema);
-export default Genre;
